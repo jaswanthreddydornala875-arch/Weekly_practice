2f734defba51ecc48f3cc3e7f0800e634a55aae1
jaswanthreddydornala875-arch <jaswanthreddydornala875@gmail.com>
2026-03-02 14:03:55 +0530

    Level2

--- Practice10.java
+++ Practice10.java
@@ -1,5 +1,6 @@
 import java.util.*;
 public class Practice10 {
+    @SuppressWarnings("ConvertToTryWithResources")
     public static void main(String[]args){
         Scanner sc=new Scanner(System.in);
         float height;

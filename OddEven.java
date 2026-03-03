e4fd194914270e883f27acbca8c20c55ad0fa7b4
jaswanthreddydornala875-arch <jaswanthreddydornala875@gmail.com>
2026-03-03 18:49:02 +0530

    Level2

---
+++ OddEven.java
@@ -0,0 +1,25 @@
+import java.util.*;
+public class OddEven {
+    public static void main(String[]args){
+        Scanner sc =new Scanner(System.in);
+        int n;
+        System.out.println("Enter the number:");
+        n=sc.nextInt();
+        if(n>0){
+            for(int i=1;i<=n;i++){
+                if(i%2==0){
+                    System.out.println(i+"is even");
+                }
+                else{
+                    System.out.println(i+"is odd");
+                }
+                
+            }
+        }
+        else{
+            System.out.println("Invalid input");
+        }
+     sc.close();   
+    }
+    
+}

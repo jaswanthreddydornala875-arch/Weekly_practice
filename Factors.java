037deda9cef315c1d52ad72b767fa018ae884d02
jaswanthreddydornala875-arch <jaswanthreddydornala875@gmail.com>
2026-03-07 17:17:16 +0530

    Level2

---
+++ Factors.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Factors {
+    public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+        int n;
+        System.out.println("Enter a number:");
+        n=sc.nextInt();
+        for(int i=1;i<=n;i++){
+            if((n%i==0)){
+                System.out.println(i+" is a factor of "+n);
+            }
+        }
+        sc.close();
+    }
+    
+}

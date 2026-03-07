907b70a90f4f6183600b7c3d251ed17f1491be3a
jaswanthreddydornala875-arch <jaswanthreddydornala875@gmail.com>
2026-03-07 17:20:09 +0530

    Level2

--- Factors.java
+++ Factors.java
@@ -5,9 +5,11 @@ public class Factors {
         int n;
         System.out.println("Enter a number:");
         n=sc.nextInt();
-        for(int i=1;i<=n;i++){
-            if((n%i==0)){
-                System.out.println(i+" is a factor of "+n);
+        if(n>0){
+            for(int i=1;i<=n;i++){
+                if((n%i==0)){
+                    System.out.println(i+" is a factor of "+n);
+                }
             }
         }
         sc.close();

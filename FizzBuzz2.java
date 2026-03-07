1acb4d2953d8d0aa53d9f7af0165dda9903823e5
jaswanthreddydornala875-arch <jaswanthreddydornala875@gmail.com>
2026-03-07 16:34:23 +0530

    Level2

---
+++ FizzBuzz2.java
@@ -0,0 +1,28 @@
+import java.util.*;
+public class FizzBuzz2{
+    public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+        int n,i=1;
+        System.out.println("Enter a number:");
+        n=sc.nextInt();
+        if(n>0){
+            while(i<=n){
+                if((i%3==0)&&(i%5==0)){
+                    System.out.println("FizzBuzz");
+                }
+                else if(i%5==0){
+                    System.out.println("Buzz");
+                }
+                else if(i%3==0){
+                    System.out.println("Fizz");
+                }
+                else{
+                    System.out.println(i);
+                }
+                i++;
+            }
+        }
+        sc.close();
+    }
+    
+}
